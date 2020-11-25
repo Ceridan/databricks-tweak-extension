86cb692b42ee9d34255d8a62d5d01c46a46d3750
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-11-26 01:21:46 +0300

    feat: Add extension options to enable or disable feature toggles

---
+++ src/js/options.js
@@ -0,0 +1,36 @@
+import Storage from './storage'
+import { FeatureTogglesStorageKey } from './features'
+
+const featureToggleElements = document.getElementsByName('feature-toggle')
+
+Storage.load(FeatureTogglesStorageKey, (fts) => {
+  if (!fts) return
+
+  featureToggleElements.forEach((element) => {
+    if (fts[element.id] || fts[element.id] === undefined) {
+      // eslint-disable-next-line no-param-reassign
+      element.checked = 'checked'
+    }
+  })
+})
+
+document.getElementById('save-options').addEventListener('click', () => {
+  const featureToggles = {}
+
+  featureToggleElements.forEach((element) => {
+    featureToggles[element.id] = !!(element.checked)
+  })
+
+  Storage.save(featureToggles, () => {
+    const statusElement = document.getElementById('options-status')
+    const error = chrome.runtime.lastError
+
+    if (error) {
+      statusElement.className = 'error'
+      statusElement.innerText = error.message
+    } else {
+      statusElement.className = 'success'
+      statusElement.innerText = 'Changes is successfully saved'
+    }
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = (_, argv) => ({
   entry: {
     content: path.resolve(__dirname, 'src/js/content.js'),
+    options: path.resolve(__dirname, 'src/js/options.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -30,9 +32,15 @@ module.exports = (_, argv) => ({
     }),
     new CopyWebpackPlugin({
       patterns: [
+        { from: 'src/css', to: 'css' },
         { from: 'src/img', to: 'img' },
         { from: 'src/manifest.json' },
       ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'options.html',
+      template: 'src/options.html',
+      chunks: ['options'],
     }),
   ],
 })
