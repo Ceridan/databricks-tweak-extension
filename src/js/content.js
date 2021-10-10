0e3a596f075e3ccb7f481b09c398cc600895a10a
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2021-10-10 23:17:05 +0300

    feat: Check Browser API on Chrome

--- src/js/content.js
+++ src/js/content.js
@@ -2,61 +2,60 @@ import browser from 'webextension-polyfill'
 import Storage from './storage'
 import { FeatureTogglesStorageKey, Features, getEnabledFeatures } from './features'
 
+let featureToggles
+let jobListFilter
+let root
+
+function getJobsPageRoot() {
+  const shadowElements = document.getElementsByTagName('databricks-jaws')
+  if (shadowElements.length === 0) return undefined
+
+  const { shadowRoot } = shadowElements[0]
+  if (!shadowRoot) return undefined
+
+  const jobsPageRoot = Array.from(shadowRoot.childNodes).find((node) => node.id === 'mfe-root')
+  return jobsPageRoot
+}
+
+function sortJobsByDesc() {
+  const columns = Array.from(root.querySelectorAll('a.header-cell'))
+  const jobColumn = columns.find((column) => column.innerText === 'Job ID')
+  jobColumn.click()
+  jobColumn.click()
+}
+
+function restoreFilterValue() {
+  const filter = root.querySelector('#input')
+
+  if (filter.value && !jobListFilter) {
+    jobListFilter = filter.value
+  } else if (!filter.value && jobListFilter) {
+    filter.value = jobListFilter
+    filter.dispatchEvent(new Event('input', { bubbles: true }))
+  }
+
+  filter.addEventListener('input', () => {
+    jobListFilter = filter.value
+  })
+}
+
+function locationHashChanged() {
+  if (document.location.hash.startsWith('#job/list')) {
+    const timerId = setInterval(() => {
+      root = getJobsPageRoot()
+
+      if (root && root.querySelectorAll('a.header-cell').length > 0) {
+        clearInterval(timerId)
+        if (featureToggles[Features.restoreFilterValue]) restoreFilterValue()
+        if (featureToggles[Features.sortJobsByDesc]) sortJobsByDesc()
+      }
+    }, 500)
+  }
+}
+
 async function contentScriptHandler() {
-  const featureToggles = getEnabledFeatures()
-
   const data = await Storage.load(FeatureTogglesStorageKey)
-  Object.assign(featureToggles, data && data[FeatureTogglesStorageKey])
-
-  let jobListFilter
-  let root
-
-  function getJobsPageRoot() {
-    const shadowElements = document.getElementsByTagName('databricks-jaws')
-    if (shadowElements.length === 0) return undefined
-
-    const { shadowRoot } = shadowElements[0]
-    if (!shadowRoot) return undefined
-
-    const jobsPageRoot = Array.from(shadowRoot.childNodes).find((node) => node.id === 'mfe-root')
-    return jobsPageRoot
-  }
-
-  function sortJobsByDesc() {
-    const columns = Array.from(root.querySelectorAll('a.header-cell'))
-    const jobColumn = columns.find((column) => column.innerText === 'Job ID')
-    jobColumn.click()
-    jobColumn.click()
-  }
-
-  function restoreFilterValue() {
-    const filter = root.querySelector('#input')
-
-    if (filter.value && !jobListFilter) {
-      jobListFilter = filter.value
-    } else if (!filter.value && jobListFilter) {
-      filter.value = jobListFilter
-      filter.dispatchEvent(new Event('input', { bubbles: true }))
-    }
-
-    filter.addEventListener('input', () => {
-      jobListFilter = filter.value
-    })
-  }
-
-  function locationHashChanged() {
-    if (document.location.hash.startsWith('#job/list')) {
-      const timerId = setInterval(() => {
-        root = getJobsPageRoot()
-
-        if (root && root.querySelectorAll('a.header-cell').length > 0) {
-          clearInterval(timerId)
-          if (featureToggles[Features.restoreFilterValue]) restoreFilterValue()
-          if (featureToggles[Features.sortJobsByDesc]) sortJobsByDesc()
-        }
-      }, 500)
-    }
-  }
+  featureToggles = getEnabledFeatures(data && data[FeatureTogglesStorageKey])
 
   window.addEventListener('load', locationHashChanged)
   window.addEventListener('hashchange', locationHashChanged)
@@ -69,4 +68,4 @@ async function contentScriptHandler() {
 
 contentScriptHandler()
   // eslint-disable-next-line no-console
-  .error((err) => console.error('Databricks Tweak extesion content error: ', err))
+  .catch((err) => console.error('Databricks Tweak extesion content error: ', err))
